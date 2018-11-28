5b258138fc016daf44d6f09c15eb92d0fb030115
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-11-28 10:21:11 -0600

    Putting together scripts for Tello EDU swarming

---
+++ swarm-box-mission.py
@@ -0,0 +1,75 @@
+# This example script demonstrates how to use Python to fly Tello in a box mission
+# This script is part of our course on Tello drone programming
+# https://learn.droneblocks.io/p/tello-drone-programming-with-python/
+
+# Import the necessary modules
+import socket
+import threading
+import time
+
+# IP and port of Tello
+tello1_address = ('192.168.0.100', 8889)
+tello2_address = ('192.168.0.102', 8889)
+
+# IP and port of local computer
+local1_address = ('', 9010)
+local2_address = ('', 9011)
+
+# Create a UDP connection that we'll send the command to
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to the local address and port
+sock1.bind(local1_address)
+sock2.bind(local2_address)
+
+# Send the message to Tello and allow for a delay in seconds
+def send(message, delay):
+  # Try to send the message otherwise print the exception
+  try:
+    sock1.sendto(message.encode(), tello1_address)
+    sock2.sendto(message.encode(), tello2_address)
+    print("Sending message: " + message)
+  except Exception as e:
+    print("Error sending: " + str(e))
+
+  # Delay for a user-defined period of time
+  time.sleep(delay)
+
+# Receive the message from Tello
+def receive():
+  # Continuously loop and listen for incoming messages
+  while True:
+    # Try to receive the message otherwise print the exception
+    try:
+      response, ip_address = sock1.recvfrom(128)
+      print("Received message: " + response.decode(encoding='utf-8'))
+    except Exception as e:
+      # If there's an error close the socket and break out of the loop
+      sock1.close()
+      sock2.close()
+      print("Error receiving: " + str(e))
+      breaka
+
+# Create and start a listening thread that runs in the background
+# This utilizes our receive functions and will continuously monitor for incoming messages
+receiveThread = threading.Thread(target=receive)
+receiveThread.daemon = True
+receiveThread.start()
+
+# Put Tello into command mode
+send("command", 3)
+
+# Send the takeoff command
+send("takeoff", 8)
+
+send("flip f", 4)
+# Land
+send("land", 5)
+
+# Print message
+print("Mission completed successfully!")
+
+# Close the socket
+sock1.close()
+sock2.close()
