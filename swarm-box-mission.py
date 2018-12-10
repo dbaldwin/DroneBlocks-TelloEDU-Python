883d83e2ff6ee439218354216d65f530f3e915b2
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-12-10 14:58:24 -0600

    Updating box mission for two Tello EDUs

--- swarm-box-mission.py
+++ swarm-box-mission.py
@@ -8,7 +8,7 @@ import threading
 import time
 
 # IP and port of Tello
-tello1_address = ('192.168.0.100', 8889)
+tello1_address = ('192.168.0.101', 8889)
 tello2_address = ('192.168.0.102', 8889)
 
 # IP and port of local computer
@@ -49,7 +49,7 @@ def receive():
       sock1.close()
       sock2.close()
       print("Error receiving: " + str(e))
-      breaka
+      break
 
 # Create and start a listening thread that runs in the background
 # This utilizes our receive functions and will continuously monitor for incoming messages
@@ -57,13 +57,28 @@ receiveThread = threading.Thread(target=receive)
 receiveThread.daemon = True
 receiveThread.start()
 
+# Each leg of the box will be 100 cm. Tello uses cm units by default.
+box_leg_distance = 100
+
+# Yaw 90 degrees
+yaw_angle = 90
+
+# Yaw clockwise (right)
+yaw_direction = "cw"
+
 # Put Tello into command mode
 send("command", 3)
 
 # Send the takeoff command
 send("takeoff", 8)
 
-send("flip f", 4)
+# Loop and create each leg of the box
+for i in range(4):
+  # Fly forward
+  send("forward " + str(box_leg_distance), 4)
+  # Yaw right
+  send("cw " + str(yaw_angle), 3)
+
 # Land
 send("land", 5)
 
